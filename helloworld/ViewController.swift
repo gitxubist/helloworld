117b939ea14bc42b4a39a317a30e15cddfa46573
Морозов Евгений <evgenii.morozov@sparm.com>
2025-01-22 18:42:36 +0300

    Initial Commit

---
+++ helloworld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Evgeniy Morozov on 22.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ helloworldTests/helloworldTests.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldTests.swift
+//  helloworldTests
+//
+//  Created by Evgeniy Morozov on 22.01.2025.
+//
+
+import Testing
+@testable import helloworld
+
+struct helloworldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
